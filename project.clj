6d88a803a2ab083142684c2ce05a6ada21efe531
Pepijn de Vos <pepijndevos@gmail.com>
2011-03-26 10:32:16 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject ant-mine "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :dependencies [[clojure "1.2.0"]])
---
+++ src/ant_mine/core.clj
@@ -0,0 +1,69 @@
+(ns ant-mine.core
+  (:import [javax.swing JFrame JPanel WindowConstants]
+           [java.awt Graphics2D Color]
+           [java.util.concurrent TimeUnit ScheduledThreadPoolExecutor])
+  (:require [clojure.java.io :as io]
+            [clojure.set :as s]))
+
+;RTree, QuadTree, Octree, bsp tree
+
+(def width 500)
+(def height 500)
+
+(def state (agent {}))
+
+(def runner (ScheduledThreadPoolExecutor. (.availableProcessors (Runtime/getRuntime))))
+
+(defrecord game-object [x y height width sprite])
+
+(def dimension (juxt (memfn getHeight) (memfn getWidth)))
+
+(defn object [x y file]
+  (let [sprite (javax.imageio.ImageIO/read (io/input-stream file))
+        [height width] (dimension sprite)]
+    (game-object. x y height width sprite)))
+
+(defn schedule
+  ([interval f]
+   (.scheduleWithFixedDelay runner f 0 interval TimeUnit/MILLISECONDS))
+  ([interval f & args]
+   (schedule interval #(apply f args))))
+
+(defn game [w h panel]
+  (doto (new JFrame)
+    (.setDefaultCloseOperation WindowConstants/DISPOSE_ON_CLOSE)
+    ;(.setUndecorated true)
+    (.setContentPane panel)
+    (.setSize w h)
+    (.setVisible true)))
+
+(defn canvas [f]
+  (proxy [JPanel] []
+    (paintComponent [g] (f g))))
+
+(declare pane)
+
+(defn pc [g]
+  (doto g
+    (.setColor (Color. (rand-int 0xffffff)))
+    (.fillRect 0 0 width height))
+  (doseq [obj @state
+          :let [v (val obj)]]
+    (.drawImage g (:sprite v) (:x v) (:y v) pane)))
+
+(def pane (canvas pc))
+(def manager (javax.swing.RepaintManager/currentManager pane))
+(game width height pane)
+
+(add-watch state :state-change
+  (fn [_ _ old new]
+    (doseq [pair (s/difference (set new) (set old))
+            obj  [(val pair) (get old (key pair))]
+            :when (not (nil? obj))]
+      (.addDirtyRegion
+        manager
+        pane
+        (:x obj)
+        (:y obj)
+        (:width obj)
+        (:height obj)))))
